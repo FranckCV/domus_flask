0106e99b55e024b80aa5aa74469ab7ed7db063cf
fabss269 <fabianapm060126@gmail.com>
2024-10-18 19:35:17 -0500

    respaldo #18-10.9

---
+++ static/js/animacion.js
@@ -0,0 +1,18 @@
+const burst = new mojs.Burst({
+    radius: { 0: 360 },
+    count: 20,
+    children: {
+        shape: 'cross',
+        stroke: 'teal',
+        strokeWidth: { 6: 0 },
+        angle: { 360: 0 },
+        radius: { 30: 5 },
+        duration: 3000
+    }
+});
+
+const timeline = new mojs.Timeline({
+    repeat: Infinity 
+})
+.add(burst)
+.play();
