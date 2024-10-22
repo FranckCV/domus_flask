696c3af8385898c4ed9c4442282699452b504f85
fabss269 <fabianapm060126@gmail.com>
2024-10-22 09:39:45 -0500

    respaldo #22-10.12

--- bd.py
+++ bd.py
@@ -3,13 +3,10 @@ import pymysql
 def obtener_conexion():
     return pymysql.connect(
                             host = 'localhost' ,
-<<<<<<< HEAD
                             # port = 3306,
                             port = 3307,
-=======
                             port = 3306,
                             # port = 3307,
->>>>>>> d0bf1fb2e0d16ecea198fb3ac70917b64d5813a6
                             user = 'root' ,
                             password = '' ,
                             db = 'bd_domus'
