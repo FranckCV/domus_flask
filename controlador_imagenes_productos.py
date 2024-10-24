22ae820d839151573bf5189cbed5b2f8b2fb0a1c
JuniorPD04 <perezdj0904@gmail.com>
2024-10-23 20:25:34 -0500

    YA FUNCA COMO PA PRESENTAR

--- controlador_imagenes_productos.py
+++ controlador_imagenes_productos.py
@@ -75,21 +75,20 @@ def actualizar_img_producto(imagen, id):
 
 
 
-def obtener_img_principal_por_producto(id):
-    conexion = obtener_conexion()
-    with conexion.cursor() as cursor:
-        sql = '''
-            SELECT 
-                count(ipr.id)
-            FROM img_producto ipr
-            where ipr.productoid = '''+str(id)+''' and ipr.imgPrincipal = 1
-            '''
-        cursor.execute(sql)
-        cant = cursor.fetchone()[0]
+# def ober_img_principal_por_producto(id):
+#     conexion = obtener_conexion()
+#     with conexion.cursor() as cursor:
+#         sql = '''
+#             SELECT 
+#                 count(ipr.id)
+#             FROM img_producto ipr
+#             where ipr.productoid = '''+str(id)+''' and ipr.imgPrincipal = 1
+#             '''
+#         cursor.execute(sql)
+#         cant = cursor.fetchone()[0]
         
-    conexion.close()
-    return cant
-
+#     conexion.close()
+#     return cant
 
 
 
